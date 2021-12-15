bef6a22aba6e5ed0d3233bb0e15797105b2035c4
Thalles Brandão <thalles-brandao@homtail.com>
2021-12-15 17:18:57 -0300

    Correção URL's

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::prefix('v1')->group(function () {
     Route::get('emails/{token}/plans', [EmailController::class, 'plan'])->name('email.plans');
     Route::get('templates/{token}', [TemplatesController::class, 'index'])->name('templates');
     Route::put('templates/{token}', [TemplatesController::class, 'update'])->name('templates.update');
-    Route::resource('emails/{token}', EmailController::class);    
+    Route::resource('emails/{token}/', EmailController::class);    
     Route::resource('sites', SitesController::class);
     Route::resource('user', UserController::class);
 });
