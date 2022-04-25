be41755693abd18396e94378afae4b5e326809dd
Thalles Brandão <thalles-brandao@homtail.com>
2022-04-25 11:56:09 -0300

    Ativando rotas

--- routes/api.php
+++ routes/api.php
@@ -24,23 +24,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function () {
-    // Route::get('emails/{token}/plans', [EmailController::class, 'plan'])->name('email.plans');
-    // Route::get('templates/{token}', [TemplatesController::class, 'index'])->name('templates');
+    Route::get('emails/{token}/plans', [EmailController::class, 'plan'])->name('email.plans');
+    Route::get('templates/{token}', [TemplatesController::class, 'index'])->name('templates');
 
-    // Route::get('emails/{token}/{email}', [EmailController::class, 'show'])->name('email.show');
-    // Route::get('emails/{token}', [EmailController::class, 'index'])->name('email.index');
-    // Route::post('emails/{token}', [EmailController::class, 'store'])->name('email.store');
-    // Route::put('emails/{token}/editar/{email}', [EmailController::class, 'update'])->name('email.update');
-    // Route::delete('emails/{token}/deletar/{email}', [EmailController::class, 'destroy'])->name('email.destroy');
+    Route::get('emails/{token}/{email}', [EmailController::class, 'show'])->name('email.show');
+    Route::get('emails/{token}', [EmailController::class, 'index'])->name('email.index');
+    Route::post('emails/{token}', [EmailController::class, 'store'])->name('email.store');
+    Route::put('emails/{token}/editar/{email}', [EmailController::class, 'update'])->name('email.update');
+    Route::delete('emails/{token}/deletar/{email}', [EmailController::class, 'destroy'])->name('email.destroy');
 
-    // Route::resource('sites', SitesController::class);
-    // Route::resource('user', UserController::class);
+    Route::resource('sites', SitesController::class);
+    Route::resource('user', UserController::class);
 
-    // Route::get('dominio/{domain}', [SitesController::class, 'domain'])->name('domain.show');
+    Route::get('dominio/{domain}', [SitesController::class, 'domain'])->name('domain.show');
 
 
-    // Route::get('projetos/{token}', [ProjectsController::class, 'index'])->name('projetos.index');
+    Route::get('projetos/{token}', [ProjectsController::class, 'index'])->name('projetos.index');
     Route::put('projetos/{token}/editar/{project}', [ProjectsController::class, 'update'])->name('projetos.update');
-    // Route::delete('projetos/{token}/deletar/{id}', [ProjectsController::class, 'destroy'])->name('projetos.destroy');
+    Route::delete('projetos/{token}/deletar/{id}', [ProjectsController::class, 'destroy'])->name('projetos.destroy');
 
 });
