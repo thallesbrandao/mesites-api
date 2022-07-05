757fb8e7160b903659e896a67ede59c1d9f945f1
Thalles Brandão <thalles-brandao@homtail.com>
2022-07-05 18:25:45 -0300

    Create Project

--- app/Http/Controllers/Api/TemplatesController.php
+++ app/Http/Controllers/Api/TemplatesController.php
@@ -15,7 +15,7 @@ class TemplatesController extends Controller
 
         $user = User::where('token', $token)->with('site')->first();
 
-        $user->site()->updateOrCreate([
+        $user->site()->update([
             'ftp' => $user->site->ftp
         ]);
 
